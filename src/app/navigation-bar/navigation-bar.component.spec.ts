bd6f1a859eeab60d8447c79a0b0b540d36e5acf8
Franck Libam <franck.libam@gmail.com>
2017-02-23 09:00:08 +0100

    activate home link when the exact url home is matched

--- src/app/navigation-bar/navigation-bar.component.spec.ts
+++ src/app/navigation-bar/navigation-bar.component.spec.ts
@@ -18,8 +18,5 @@ describe('NavigationBarComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });
