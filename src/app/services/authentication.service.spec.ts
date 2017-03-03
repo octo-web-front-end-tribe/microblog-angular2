e9f4691b63930c5aee496c494550bd62b6dc3e4b
Franck Libam <franck.libam@gmail.com>
2017-03-03 08:53:07 +0100

    refactor authentication service spec file

--- src/app/services/authentication.service.spec.ts
+++ src/app/services/authentication.service.spec.ts
@@ -1,19 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { AuthenticationService } from './authentication.service.ts';
-import { LocalStorageModule } from 'angular-2-local-storage';
 import { RouterTestingModule } from '@angular/router/testing';
+import {LocalStorageService} from "angular-2-local-storage/dist/index";
 
 describe('AuthenticationService', () => {
+
+  let localStorageServiceStub;
+
   beforeEach(() => {
+    localStorageServiceStub = {
+      set(){},
+      get(){}
+    };
     TestBed.configureTestingModule({
       imports: [
-        LocalStorageModule.withConfig({
-          prefix: 'app-root',
-          storageType: 'localStorage'
-        }),
         RouterTestingModule
       ],
-      providers: [AuthenticationService]
+      providers: [
+        AuthenticationService,
+        {provide: LocalStorageService, useValue: localStorageServiceStub}
+      ]
     });
   });
 
@@ -22,8 +28,26 @@ describe('AuthenticationService', () => {
   }));
 
 
-  it("Should put login in local storage", inject([AuthenticationService], (service:AuthenticationService) => {
-    //TODO write test
+  xit('#login should set user value into localStorage', inject([AuthenticationService], (service:AuthenticationService) => {
+    const spy = spyOn(localStorageServiceStub, 'set');
+    service.login('abc');
+    expect(spy).toHaveBeenCalledWith('user', 'abc');
+  }));
+
+  xit('#getUser should set user value into localStorage', inject([AuthenticationService], (service:AuthenticationService) => {
+    const spy = spyOn(localStorageServiceStub, 'get').and.returnValue('abc');
+    expect(service.getUser()).toBe('abc');
+    expect(spy).toHaveBeenCalledWith('user', 'abc');
+  }));
+
+  xit('#isLoggedIn should return true if user is set inside localStorage', inject([AuthenticationService], (service:AuthenticationService) => {
+    const spy = spyOn(localStorageServiceStub, 'get').and.returnValue('abc');
+    expect(service.isLoggedIn()).toBe(true);
+  }));
+
+  xit('#isLoggedIn should return false if user is not set inside localStorage', inject([AuthenticationService], (service:AuthenticationService) => {
+    const spy = spyOn(localStorageServiceStub, 'get').and.returnValue(undefined);
+    expect(service.isLoggedIn()).toBe(false);
   }));
 
 });
