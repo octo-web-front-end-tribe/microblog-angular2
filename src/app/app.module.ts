bc3f090efbbdb8de33e6822c2e696914d08082f9
Franck Libam <franck.libam@gmail.com>
2017-04-07 01:23:34 +0200

    init translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {HttpModule, Http} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MessageListComponent } from './components/message-list/message-list.component';
@@ -18,6 +18,13 @@ import {AuthenticationService} from "./services/authentication.service";
 import { LocalStorageModule } from 'angular-2-local-storage';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { ProtectedDirective } from './directives/protected.directive';
+
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: Http) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -42,6 +49,13 @@ import { ProtectedDirective } from './directives/protected.directive';
     LocalStorageModule.withConfig({
       prefix: 'app-root',
       storageType: 'localStorage'
+    }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
     })
   ],
   providers: [MessageService, AuthenticationService],
